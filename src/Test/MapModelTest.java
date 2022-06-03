74743b73b65183af94e1a21150d739832711e368
chansooo <kcs05008@gmail.com>
2022-06-03 17:03:01 +0900

    [feat] make MapModelTest

---
+++ src/Test/MapModelTest.java
@@ -0,0 +1,83 @@
+import Model.MapCell;
+import Model.MapModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class MapModelTest {
+    @Test
+    @DisplayName("map 생성자 테스트 실행")
+    void initializeMapTest() throws IOException {
+        MapModel a = new MapModel(1);
+    }
+
+    @Test
+    @DisplayName("default.map테스트 실행")
+    void getMapTest0() throws IOException {
+        String testMap[] = new String[]{
+                "XXXXCCHXXXXXX",
+                "TCCXPXCXXXXXX",
+                "XXBXbXCXXXXXX",
+                "XXSXCXCXCCHXX",
+                "XXCXCXBXbXCXX",
+                "XXCXCXCXCXCXE",
+                "XXCXBXbXCXCXC",
+                "XXCCHXCXCXPXC",
+                "XXXXXXSXCXBXb",
+                "XXXXXXPXBXbXC",
+                "XXXXXXCXCXCXC",
+                "XXXXXXCXHXHXP",
+                "XXXXXXCCCXCCS"
+        };
+        MapModel a = new MapModel(0);
+        MapCell[][] b = a.getMap();
+        int xCut = a.getxCut();
+        int yCut = a.getyCut();
+        for(int i=0;i< yCut;i++){
+            for(int j =0; j<xCut;j++){
+                System.out.print(b[i][j].cellState);
+            }
+            System.out.println();
+        }
+        for(int i=0;i<yCut;i++){
+            for(int j=0; j<xCut;j++){
+                assertEquals(testMap[i].charAt(j), b[i][j].cellState);
+            }
+        }
+
+    }
+
+    @Test
+    @DisplayName("another.map테스트 실행")
+    void getMapTest1() throws IOException {
+        String testMap[] = new String[]{
+                "TCCXE",
+                "XXBXb",
+                "XXSXC",
+                "XXBXb",
+                "XXCXC",
+                "XXCXC",
+                "XXCCH"
+        };
+        MapModel a = new MapModel(1);
+        MapCell[][] b = a.getMap();
+        int xCut = a.getxCut();
+        int yCut = a.getyCut();
+        for(int i=0;i< yCut;i++){
+            for(int j =0; j<xCut;j++){
+                System.out.print(b[i][j].cellState);
+            }
+            System.out.println();
+        }
+        for(int i=0;i<yCut;i++){
+            for(int j=0; j<xCut;j++){
+                assertEquals(testMap[i].charAt(j), b[i][j].cellState);
+            }
+        }
+
+    }
+}
