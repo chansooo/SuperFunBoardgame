fb42c74922803a9201e9ffcd7816da4388f0ff72
chansooo <kcs05008@gmail.com>
2022-06-05 16:30:19 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import Controller.CLIController;
+import Controller.GUIController;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        System.out.println("게임 뷰를 선택하세요.");
+        System.out.println("GUI: 1, CLI: 2 : ");
 
+        Scanner sc = new Scanner(System.in);
+        int GameType = sc.nextInt();
+
+        if(GameType == 1){
+            new GUIController();
+        }else if(GameType == 2){
+            new CLIController();
+        }
     }
 }
