c8d234991904208fcb02881d17151efe1ee82271
chansooo <kcs05008@gmail.com>
2022-06-07 00:23:38 +0900

    [sub] delete GUI

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,8 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("게임 뷰를 선택하세요.");
-        System.out.println("GUI: 1, CLI: 2 : ");
+    public static void main(String[] args) throws Exception {
+        new CLIController();
 
-        Scanner sc = new Scanner(System.in);
-        int GameType = sc.nextInt();
-
-        if(GameType == 1){
-            new GUIController();
-        }else if(GameType == 2){
-            new CLIController();
-        }
     }
 }
