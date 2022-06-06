92e512dda4413f4ad6301bf532c9f13336c803de
chansooo <kcs05008@gmail.com>
2022-06-07 00:25:44 +0900

    [feat] add playerTest case

--- src/Test/PlayerModelTest.java
+++ src/Test/PlayerModelTest.java
@@ -1,7 +1,7 @@
-import Model.MapCell;
+import Model.MapCellModel;
 import Model.MapModel;
 import Model.PlayerModel;
-import Model.Position;
+import Model.PositionModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,8 +15,8 @@ public class PlayerModelTest {
     @DisplayName("Player 생성자 테스트 실행")
     void initializePlayerTest() throws IOException {
         MapModel a = new MapModel(1);
-        MapCell[][] map = a.getMap();
-        Position startpos = new Position(1,0);
+        MapCellModel[][] map = a.getMap();
+        PositionModel startpos = new PositionModel(1,0);
         PlayerModel p = new PlayerModel(startpos, map);
     }
 
@@ -24,10 +24,10 @@ public class PlayerModelTest {
     @DisplayName("stayPlayer 테스트 실행")
     void stayPlayerTest() throws Exception {
         MapModel a = new MapModel(0);
-        MapCell[][] map = a.getMap();
-        Position startpos = a.getStartPos();
+        MapCellModel[][] map = a.getMap();
+        PositionModel startpos = a.getStartPos();
         PlayerModel p = new PlayerModel(startpos, map);
-        Position temp = p.stayPlayer();
+        PositionModel temp = p.stayPlayer();
         assertEquals(temp.dx, startpos.dx);
         assertEquals(temp.dy, startpos.dy);
     }
@@ -38,10 +38,10 @@ public class PlayerModelTest {
     @DisplayName("movePlayer 테스트 실행: 끝난 유저X")
     void movePlayerTest() throws Exception {
         MapModel a = new MapModel(0);
-        MapCell[][] map = a.getMap();
-        Position startpos = a.getStartPos();
+        MapCellModel[][] map = a.getMap();
+        PositionModel startpos = a.getStartPos();
         PlayerModel p = new PlayerModel(startpos, map);
-        Position temp = p.movePlayer("RLR", 3, false);
+        PositionModel temp = p.movePlayer("RLR", 3, false);
         assertEquals(temp.dx, 1);
         assertEquals(temp.dx, 1);
     }
@@ -50,11 +50,41 @@ public class PlayerModelTest {
     @DisplayName("movePlayer 테스트 실행: 끝난 유저O")
     void movePlayerTest1() throws Exception {
         MapModel a = new MapModel(0);
-        MapCell[][] map = a.getMap();
-        Position startpos = a.getStartPos();
+        MapCellModel[][] map = a.getMap();
+        PositionModel startpos = a.getStartPos();
         PlayerModel p = new PlayerModel(startpos, map);
-        Position temp = p.movePlayer("RRD", 3, true);
-        assertEquals(temp.dx, 2);
-        assertEquals(temp.dx, 2);
+        PositionModel temp = p.movePlayer("R", 1, true);
+        assertEquals(temp.dx, 1);
+        //assertEquals(temp.dy, startpos.dy);
     }
+
+    @Test
+    @DisplayName("movePlayer 테스트 실행: bridge 통과")
+    void movePlayerTest2() throws Exception {
+        MapModel a = new MapModel(1);
+        MapCellModel[][] map = a.getMap();
+        PositionModel startpos = a.getStartPos();
+        PlayerModel p = new PlayerModel(startpos, map);
+        PositionModel temp = p.movePlayer("RRDRR", 5, false);
+        assertEquals(temp.dx, 4);
+        assertEquals(temp.dy, 1);
+    }
+
+    @Test
+    @DisplayName("movePlayer 테스트 실행: command 잘못 입력")
+    void movePlayerTest3() throws Exception {
+        MapModel a = new MapModel(1);
+        MapCellModel[][] map = a.getMap();
+        PositionModel startpos = a.getStartPos();
+        PlayerModel p = new PlayerModel(startpos, map);
+        PositionModel temp = new PositionModel(1,0);
+        try{
+            temp = p.movePlayer("RRR", 3, false);
+        } catch (Exception e){
+            assertEquals(p.getPosition(), startpos);
+        }
+
+    }
+
+
 }
