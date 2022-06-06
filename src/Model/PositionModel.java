15f9328c3dbd97aed20702f06c983a99a4616b49
chansooo <kcs05008@gmail.com>
2022-06-07 00:24:31 +0900

    [feat] make position class

---
+++ src/Model/PositionModel.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class PositionModel {
+    public int dx;
+    public int dy;
+
+    public PositionModel(int dy, int dx) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+}
